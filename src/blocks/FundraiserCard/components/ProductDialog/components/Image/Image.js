eac3b81f64a6764192b9748ef9b3173be78f67f4
Abhishek Chauhan <ac12644@gmail.com>
2022-07-15 14:50:18 +0200

    clear code

--- src/blocks/FundraiserCard/components/ProductDialog/components/Image/Image.js
+++ src/blocks/FundraiserCard/components/ProductDialog/components/Image/Image.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 
 const Image = ({ images, name }) => {
   return (
