71eb78523ccadf894817f5b11037981b9e876788
Abhishek Chauhan <ac12644@gmail.com>
2022-10-18 16:32:33 +0200

    removed coinbase wallet

--- src/web3/Login/Login.js
+++ src/web3/Login/Login.js
@@ -1,23 +1,155 @@
-import { useContext } from 'react';
+import { useCallback, useEffect, useReducer, useState } from 'react';
 import { IconButton } from '@mui/material';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import Account from './components/Account';
-import { WalletContext } from '../Web3Auth/config';
+
+import { ellipseAddress, getChainData } from './lib/utilities';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { providers } from 'ethers';
+import Web3Modal from 'web3modal';
+
+export const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: process.env.INFURA_API_KEY, // required
+    },
+  },
+};
+let web3Modal;
+if (typeof window !== 'undefined') {
+  web3Modal = new Web3Modal({
+    network: 'mainnet', // optional
+    cacheProvider: true,
+    providerOptions, // required
+  });
+}
+
+const initialState = {
+  provider: null,
+  web3Provider: null,
+  address: null,
+  chainId: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_WEB3_PROVIDER':
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case 'SET_ADDRESS':
+      return {
+        ...state,
+        address: action.address,
+      };
+    case 'SET_CHAIN_ID':
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case 'RESET_WEB3_PROVIDER':
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
 
 export const Login = () => {
-  const { login, logout, accountAddress, connected } =
-    useContext(WalletContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { provider, web3Provider, address, chainId } = state;
+
+  const connect = useCallback(async function () {
+    const provider = await web3Modal.connect();
+
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+
+    const network = await web3Provider.getNetwork();
+
+    dispatch({
+      type: 'SET_WEB3_PROVIDER',
+      provider,
+      web3Provider,
+      address,
+      chainId: network.chainId,
+    });
+  }, []);
+
+  const disconnect = useCallback(
+    async function () {
+      await web3Modal.clearCachedProvider();
+      if (provider?.disconnect && typeof provider.disconnect === 'function') {
+        await provider.disconnect();
+      }
+      dispatch({
+        type: 'RESET_WEB3_PROVIDER',
+      });
+      setAnchorEl(null);
+    },
+    [provider],
+  );
+
+  // A `provider` should come with EIP-1193 events. We'll listen for those events
+  // here so that when a user switches accounts or networks, we can update the
+  // local React state with that new information.
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts) => {
+        // eslint-disable-next-line no-console
+        console.log('accountsChanged', accounts);
+        dispatch({
+          type: 'SET_ADDRESS',
+          address: accounts[0],
+        });
+      };
+
+      // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+      const handleChainChanged = (_hexChainId) => {
+        window.location.reload();
+      };
+
+      const handleDisconnect = (error) => {
+        // eslint-disable-next-line no-console
+        console.log('disconnect', error);
+        disconnect();
+      };
+
+      provider.on('accountsChanged', handleAccountsChanged);
+      provider.on('chainChanged', handleChainChanged);
+      provider.on('disconnect', handleDisconnect);
+
+      // Subscription Cleanup
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener('accountsChanged', handleAccountsChanged);
+          provider.removeListener('chainChanged', handleChainChanged);
+          provider.removeListener('disconnect', handleDisconnect);
+        }
+      };
+    }
+  }, [provider, disconnect]);
+
+  const chainData = getChainData(chainId);
 
   return (
     <div className="container">
-      {connected ? (
+      {web3Provider ? (
         <Account
           icon="https://firebasestorage.googleapis.com/v0/b/virtualground-meta.appspot.com/o/nft%2Ficon.png?alt=media&token=51904b60-2b20-47aa-9502-67f4aabc8061"
-          address={accountAddress}
-          handleLogout={logout}
+          address={ellipseAddress(address)}
+          handleLogout={disconnect}
         />
       ) : (
-        <IconButton color="primary" onClick={login} size="medium">
+        <IconButton color="primary" onClick={connect} size="medium">
           <AccountBalanceWalletIcon fontSize="large" />
         </IconButton>
       )}
