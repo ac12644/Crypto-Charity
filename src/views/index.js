8b7377d795739f5ccd2361e2ea94b4f8e00a94d8
Abhishek Chauhan <ac12644@gmail.com>
2022-10-27 23:42:25 +0200

    typo fix

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from './Home';
 export { default as Create } from './Create';
 export { default as Projects } from './Projects';
-import { default as Receipt } from './Receipt';
+export { default as Receipt } from './Receipt';
 export { default as NotFound } from './NotFound';
 export { default as ServerError } from './ServerError';
